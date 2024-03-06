75e68dd9588faa4032f8f43406102523e6c600de
alexuma <alexumadw@gmail.com>
2024-03-06 06:40:51 -0300

    modulos ES6

---
+++ node_js/02_modulos/app.mjs
@@ -0,0 +1,8 @@
+import calculadora from "./calculadora.mjs";
+const c = console.log;
+
+c(calculadora.sumar(2, 3));
+c(calculadora.restar(2, 3));
+c(calculadora.multiplicar(2, 3));
+c(calculadora.dividir(2, 3));
+c(calculadora.modulo(2, 3));
---
+++ node_js/02_modulos/calculadora.mjs
@@ -0,0 +1,24 @@
+const sumar = (a, b) => {
+    return a + b;
+  },
+  restar = (a, b) => {
+    return a - b;
+  },
+  dividir = (a, b) => {
+    return a / b;
+  },
+  multiplicar = (a, b) => {
+    return a * b;
+  },
+  modulo = (a, b) => {
+    return a % b;
+  };
+
+const calculadora = {
+  sumar,
+  restar,
+  dividir,
+  multiplicar,
+  modulo,
+};
+export default calculadora;
